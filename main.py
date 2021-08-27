c10ad2628169264dff5dfbc49d32921f47abfc5d
Bingsu <ks2515@naver.com>
2021-08-27 20:34:10 +0900

    안쓰는 부분을 지우고 async를 추가함

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from typing import Optional
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from predict import predict
+
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+@app.get("/")
+async def root():
+    return "악플탐지 API의 메인 페이지입니다. 예측을 하려는 경우 /text/{문장}으로 요청을 보내시길 바랍니다."
 
 
 @app.get("/text/{text}")
-def read_item(text: str):
+async def read_item(text: str):
     return {"text": text, "embedding": predict(text)}
