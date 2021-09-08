962f74a346e74fd67c88f4e41abb06b44149be75
Bingsu <ks2515@naver.com>
2021-09-08 14:31:15 +0900

    KoCharELECTRA 기반으로 재설정

--- model/predict.py
+++ model/predict.py
@@ -1,21 +1,23 @@
-import pathlib
 import torch
-from transformers import ElectraForSequenceClassification, ElectraTokenizer
+from transformers import ElectraForSequenceClassification
 
 # kocharelectra tokenizer
-# from tokenizer.tokenization_kocharelectra import KoCharElectraTokenizer
+from model.tokenizer import KoCharElectraTokenizer
 
-CURRENT_PATH = pathlib.Path().resolve()
-MODEL_PATH = CURRENT_PATH / "model/model"
+MODEL_PATH = "./model/model"
 
 model = ElectraForSequenceClassification.from_pretrained(MODEL_PATH)
-tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-small-v3-discriminator")
+tokenizer = KoCharElectraTokenizer.from_pretrained(
+    "monologg/kocharelectra-small-discriminator", model_max_length=512
+)
 model.eval()
 
 
 def predict(text: str):
     with torch.no_grad():
-        tokens = tokenizer(text, padding="max_length", truncation=True, return_tensors="pt")
+        tokens = tokenizer(
+            text, padding="max_length", truncation=True, return_tensors="pt"
+        )
         output = model(**tokens)
         logits = output.logits
         pred = logits.argmax().item()
