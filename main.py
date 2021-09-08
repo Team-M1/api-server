c79d07f64597689312f7d6d1b2792d702aea0119
Bingsu <ks2515@naver.com>
2021-09-08 10:45:44 +0900

    text를 predict로 변경, 리턴값을 딕셔너리로 이쁘게 담음

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "악플탐지 API의 메인 페이지입니다. 예측을 하려는 경우 /text/{문장}으로 요청을 보내시길 바랍니다."
+    return "악플탐지 API의 메인 페이지입니다. 예측을 하려는 경우 /predict/{문장}으로 요청을 보내시길 바랍니다."
 
 
-@app.get("/text/{text}")
+@app.get("/predict/{text}")
 async def read_item(text: str):
-    return {"text": text, "embedding": predict(text)}
+    d = {"text": text}
+    d.update(predict(text))
+    return d
