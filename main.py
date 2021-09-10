6953a8a84fc222790aea7faf9da16bb6428d0014
Bingsu <ks2515@naver.com>
2021-09-10 12:14:43 +0900

    POST 요청도 받게 변경

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from model.predict import predict
 
@@ -6,14 +7,29 @@ from model.predict import predict
 app = FastAPI()
 
 
+class Text(BaseModel):
+    text: str
+
+
 @app.get("/")
 async def root():
-    return "악플탐지 API의 메인 페이지입니다. 예측을 하려는 경우 /predict/{문장}으로 요청을 보내시길 바랍니다."
+    message = """악플탐지 API의 메인 페이지입니다.
+예측을 하려면 /predict/{문장}으로 GET 요청을 보내거나,
+/predict에 {'text': 문장}으로 POST 요청을 보내길 바랍니다."""
+    return message.replace("\n", " ")
 
 
 @app.get("/predict/{text}")
-async def inference(text: str):
+async def inference_get(text: str):
     d = {"text": text}
     pred = await predict(text)
+    d.update(pred)
+    return d
+
+
+@app.post("/predict")
+async def inference_post(text: Text):
+    d = {"text": text.text}
+    pred = await predict(text.text)
     d.update(pred)
     return d
